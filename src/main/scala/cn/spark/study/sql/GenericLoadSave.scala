58f3cb1307885ac1b68823e57af3a76691eeafa9
王树兵 <wang.sb@belle.com.cn>
2018-11-21 14:46:42 +0800

    1.0.0

---
+++ src/main/scala/cn/spark/study/sql/GenericLoadSave.scala
@@ -0,0 +1,22 @@
+package cn.spark.study.sql
+
+
+import org.apache.spark.sql.SparkSession
+
+
+/**
+  * @author king
+  */
+object GenericLoadSave {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("GenericLoadSave")
+      .getOrCreate()
+
+    val usersDF = spark.read.load("hdfs://spark1:9000/users.parquet")
+    usersDF.write.save("hdfs://spark1:9000/namesAndFavColors_scala")
+  }
+
+}
